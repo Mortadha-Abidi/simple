1de9379bd9800adbab79c049a23463e58daed014
Mortadha-Abidi <mortadha.abidi09@gmail.com>
2023-05-23 22:43:05 +0100

    navabar and design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { Navbar } from './component/Navbar';
+import { BrowserRouter, Route } from "react-router-dom";
+import Contact from './pages/contact/Contact.js'
+import Skills from './pages/skills/Skills.js'
+import Home from './pages/home/Home.js'
+import Project from './pages/project/Project.js'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Navbar/>
+      <Route exact path="/" component={Home} />
+        <Route path="/skills" component={Skills} />
+        <Route path="/projects" component={Project} />
+        <Route path="/contact" component={Contact} />
+      </BrowserRouter>
     </div>
   );
 }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import '../component/navabr.css'
+import {FaBars} from "react-icons/fa"
 export const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+       
+            <div className='container'>
+                <div className='logo'>
+                <h1>logo</h1>
+                </div>
+                <div className='navigation'>
+                <ul className='nav-links'>
+                    <li>Home</li>
+                    <li>Skills</li>
+                    <li>Project</li>
+                    <li>contact</li>
+                </ul>
+                <div>
+                <FaBars/>
+                </div>
+                </div>
+            </div>
+      
     )
 }
