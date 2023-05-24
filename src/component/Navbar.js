456ebd4e5c160d57306607e39e6f21465475c19a
Mortadha-Abidi <mortadha.abidi09@gmail.com>
2023-05-25 00:29:36 +0100

    change style to grid

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,25 +1,26 @@
 import React from 'react'
 import '../component/navabr.css'
-import {FaBars} from "react-icons/fa"
+// import {FaBars} from "react-icons/fa"
 import { Link, NavLink } from "react-router-dom";
 export const Navbar = () => {
     return (
        
             <div className='container'>
                 <div className='logo'>
-                <Link to="/"><h1>logo</h1></Link>
+                <Link to="/" activeClassName="active"><h1>logo</h1></Link>
                 </div>
                 <div className='navigation'>
-                <ul className='nav-links'>
+                <ul>
                   <NavLink to="/">  <li>Home</li></NavLink>
                   <NavLink to="/skills">  <li>Skills</li></NavLink>
                   <NavLink to="/projects">   <li>Project</li></NavLink>
                   <NavLink to="/contact"><li>contact</li></NavLink> 
                 </ul>
-                <div>
+               {/* <div>
                 <FaBars/>
+                </div> */}
                 </div>
-                </div>
+                 
             </div>
       
     )
